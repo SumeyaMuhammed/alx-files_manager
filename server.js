3e1b20b19f046975a32ca088ef265ab4bb9e3e49
Sumeya Muhammed <159241443+SumeyaMuhammed@users.noreply.github.com>
2024-12-12 20:26:26 -1100

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-import express from 'express';
-import controllerRouting from './routes/index';
+const express = require('express');
+const routes = require('./routes');
+const { PORT = 5000 } = process.env;
 
 const app = express();
-const port = process.env.PORT || 5000;
 
-app.use(express.json());
-controllerRouting(app);
+app.use('/', routes);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
-
-export default app;
